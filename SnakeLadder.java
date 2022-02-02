896c61b696fb1a133834ebfb0f17e72f1465b52a
ABHILASHKOLTE <abhilash.kolte@gmail.com>
2022-02-02 22:44:59 +0530

    rollDieTOgt1to6

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -5,11 +5,15 @@ public class SnakeLadder {
     int playerPosition = 0;
 
     //Method to Show Player Current Position
-    private void showPosition() {
+    public void showPosition() {
         //Displaying Player Position
         System.out.println("Player Starting Position = " + playerPosition);
     }
-
+    //Method to Roll the Die and Get No between 1 to 6
+    public int rollDie() {
+        int dieNo = (int) Math.floor(Math.random() * 10)%6+1;
+        return dieNo;
+    };
     public static void main(String[] args) {
         //Displaying Welcome Message
         System.out.println("Welcome to Snake and Ladder Simulation ab Abhilash ");
@@ -19,5 +23,8 @@ public class SnakeLadder {
 
         //Showing Player Position
         player1.showPosition();
+
+        //Show Die Rolling No
+        System.out.println("Die Number for Player : "+ player1.rollDie());
     }
 }
